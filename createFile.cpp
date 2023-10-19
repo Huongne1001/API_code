3ff402d5d41e2f5785b990807f4f2c5a83b33da5
Huongne1001 <130726286+Huongne1001@users.noreply.github.com>
2023-10-20 00:20:31 +0700

    Add files via upload

---
+++ createFile.cpp
@@ -0,0 +1,28 @@
+﻿#include <windows.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+    //khai báo một con trỏ kiểu LPCWSTR với giá trị là đường dẫn của tệp mới
+    LPCWSTR path = L"E:\\Hello\\haha.txt"; 
+
+    // Tạo một tệp mới sử dụng CreateFile API
+    HANDLE hFile = (CreateFile(
+        path,    //Tên của tệp mới.
+        GENERIC_READ|GENERIC_WRITE, //Quyền truy cập mong muốn cho tệp mới
+        FILE_SHARE_READ,        //Chế độ chia sẻ cho tệp mới.         
+        NULL,               //Thông tin bảo mật cho tệp mới
+        CREATE_NEW,         // Cách thức tạo tệp mới.
+        FILE_ATTRIBUTE_NORMAL, //Thuộc tính cho tệp mới.
+        NULL));                //Tệp mẫu
+    
+    
+    if (hFile == INVALID_HANDLE_VALUE) {
+        std::cout << "Khong the tao tep! Loi: " << GetLastError() << std::endl;
+    }
+    else {
+        std::cout << "Tep da duoc tao thanh cong!" << std::endl;
+        CloseHandle(hFile);
+    }
+    return 0;
+}
